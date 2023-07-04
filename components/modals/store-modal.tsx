f4e188a2e56d2167625eac32ad1a2b6d6f1db272
Andy Ramaroson <andyramaroson@gmail.com>
2023-07-04 19:16:49 +0200

    [enhancement] add package and  create Form component in a root page

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,10 +1,35 @@
 "use client"
+import * as z from "zod" ;
+import { useForm } from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod"
 
 import { useStoreModal } from "@/hooks/use-store-modal"
 import { Modal } from "@/components/ui/modal"
 
+import { Form, FormField, FormItem, FormLabel, FormControl } from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+
+import { useState } from "react";
+
+const formSchema = z.object({
+  name: z.string().min(3)
+})
+
 export const StoreModal = () => {
   const storeModal = useStoreModal();
+  const [loading, setLoading]= useState(false)
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name:"",
+    },
+  })
+
+  const onSubmit = async (value: z.infer<typeof formSchema>) => {
+    console.log(value);    
+  }
 
   return (
     <Modal 
@@ -13,6 +38,28 @@ export const StoreModal = () => {
       isOpen={storeModal.isOpen} 
       onClose={storeModal.onClose}
     >
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+            <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input disabled={loading}
+                      placeholder="E-commerce" {...field}></Input>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+        </div>
+      </div>
+
       Future Create Store Form
     </Modal>
   )
