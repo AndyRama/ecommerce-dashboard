8b0ae0c809b03d5b1035234a55f5e3e74d1bdc33
Andy Ramaroson <andyramaroson@gmail.com>
2023-07-07 16:26:20 +0200

    [enhancement] create form content, add input and btn submit

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,14 +1,26 @@
 "use client"
 
 import * as z from 'zod';
-import { Button } from "@/components/ui/button";
-import { Heading } from "@/components/ui/heading";
 
 import { Store } from "@prisma/client";
-import { Trash } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
+
+import { Trash } from "lucide-react";
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+
+import { 
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl
+} from '@/components/ui/form'
+import { useState } from 'react';
+
 
 interface SettingsFormProps {
   initialData: Store 
@@ -24,10 +36,18 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
   initialData
 }) => {
 
+  const [open, setOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
+ 
   const form = useForm<SettingsFormValues>({
-    resolver: zodResolver(formSchema)
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData
   })
-  
+
+  const onSubmit = async (data: SettingsFormValues) => {
+    console.log(data);    
+  }
+
   return (
     <>
       <div className="flex items-center justify-between">
@@ -37,15 +57,34 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
           />
         <Button
           variant="destructive"
-          size="sm"
-          onClick={()=>{
-            
-          }}
+          size="icon"
+          onClick={()=>{}}
           >
           <Trash className="h-4 w-4"/>
         </Button>
       </div>
       <Separator/>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                 <FormControl>
+                    <Input disabled={loading} placeholder='Store Name' {...field}/>
+                 </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className='ml-auto'type='submit'>
+            Save changes
+          </Button>
+        </form>
+      </Form>
     </>
   );
 };
