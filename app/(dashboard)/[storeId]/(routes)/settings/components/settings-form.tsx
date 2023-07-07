d5c182e81f86c060d4bf166be8d3bccd99623d1e
Andy Ramaroson <andyramaroson@gmail.com>
2023-07-07 14:52:55 +0200

    [enhancement] add separator, connect to formschema

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,18 +1,33 @@
 "use client"
 
+import * as z from 'zod';
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 
 import { Store } from "@prisma/client";
 import { Trash } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 interface SettingsFormProps {
   initialData: Store 
 }
 
+const formSchema = z.object({
+  name: z.string().min(3)
+})
+
+type SettingsFormValues = z.infer<typeof formSchema>;
+
 export const SettingsForm: React.FC<SettingsFormProps> = ({
   initialData
 }) => {
+
+  const form = useForm<SettingsFormValues>({
+    resolver: zodResolver(formSchema)
+  })
+  
   return (
     <>
       <div className="flex items-center justify-between">
@@ -30,6 +45,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
           <Trash className="h-4 w-4"/>
         </Button>
       </div>
+      <Separator/>
     </>
   );
 };
