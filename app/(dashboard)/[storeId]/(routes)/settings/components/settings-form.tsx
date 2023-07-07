3c592b4c3333f0f27ad5f6d79d916183e47104ef
Andy Ramaroson <andyramaroson@gmail.com>
2023-07-07 14:19:01 +0200

    [enhancement] add separator component, create btn delete and add style

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,7 +1,10 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
+
 import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
 
 interface SettingsFormProps {
   initialData: Store 
@@ -11,12 +14,22 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
   initialData
 }) => {
   return (
-    <div className="flex items-center justify-between">
-      <Heading 
-        title="Settings"
-        description="Manage store preferences"      
-      />
-      Settings Form
-    </div>
+    <>
+      <div className="flex items-center justify-between">
+        <Heading 
+          title="Settings"
+          description="Manage store preferences"      
+          />
+        <Button
+          variant="destructive"
+          size="sm"
+          onClick={()=>{
+            
+          }}
+          >
+          <Trash className="h-4 w-4"/>
+        </Button>
+      </div>
+    </>
   );
 };
