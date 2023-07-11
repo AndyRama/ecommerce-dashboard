91afcda70d224f4bedb7330a909bdf6514cf5c22
Andy Ramaroson <andyramaroson@gmail.com>
2023-07-11 15:06:34 +0200

    [Fix] error display Badge

--- components/ui/api-alert.tsx
+++ components/ui/api-alert.tsx
@@ -21,13 +21,13 @@ const variantMap: Record<ApiAlertProps["variant"], string> = {
 export const ApiAlert: React.FC<ApiAlertProps> = ({
   title,
   description,
-  variant = "public"
+  variant = "public",
 }) => {
   return (
     <Alert>
       <Server className="h-4 w-4"/>
       <AlertTitle className="flex items-center gap-x-2">{title}
-        <Badge> {textMap}</Badge>
+        <Badge></Badge>
       </AlertTitle>
       <p>{description}</p>
     </Alert>
