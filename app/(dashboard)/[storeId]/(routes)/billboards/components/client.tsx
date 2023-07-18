9d763e673d09f1c1d7131bbfcb4a0a944027d61b
Andy Ramaroson <andyramaroson@gmail.com>
2023-07-18 23:48:03 +0200

    [enhancement] add react Table

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -5,15 +5,22 @@ import { Button } from "@/components/ui/button"
 import { Heading} from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
 import { useRouter, useParams } from "next/navigation"
+import { Billboard } from "@prisma/client"
 
-export const BillboardClient = () => {
+interface BillboardClientProps {
+  data: Billboard[]
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({
+  data
+}) => {
   const router = useRouter();
   const params = useParams()
   return (
     <>    
       <div className="flex items-center justify-between">
         <Heading
-          title="BillBoards (0)"
+          title={`BillBoards (${data.length})`}
           description="Manage billboards for your store "
         />
         <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
