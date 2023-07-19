90b52311eb298676456bbdc47f1831f7c488d525
Andy Ramaroson <andyramaroson@gmail.com>
2023-07-19 22:43:57 +0200

    [enhancement] add formatedBillboard and date-fns

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,5 +1,7 @@
+import { format } from "date-fns"
 import prismadb from "@/lib/prismadb"
 import {BillboardClient} from "./components/client"
+import { BillboardColumn } from "./components/columns"
 
 const BillBoardsPage = async ({
   params
@@ -14,10 +16,16 @@ const BillBoardsPage = async ({
       createdAt: 'desc'
     }
   })
+
+  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+    id: item.id,
+    label: item.label,
+    createdAt: format(item.createdAt, "MMMM do, yyyy")
+  }))
   return(
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={billboards}/>
+        <BillboardClient data={formattedBillboards}/>
       </div>
     </div>
   )
