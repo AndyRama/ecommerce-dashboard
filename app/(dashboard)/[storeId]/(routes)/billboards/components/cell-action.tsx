c17c4408e19038c3abbf4478b94417d9d4d5729a
Andy Ramaroson <andyramaroson@gmail.com>
2023-07-20 23:25:52 +0200

    [enhancement] add dropdown menu

--- app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -1,14 +1,25 @@
 "use client"
+import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { BillboardColumn } from "./columns"
+import { Button } from "@/components/ui/button"
+import { MoreHorizontal } from "lucide-react"
 
 interface CellActionProps {
   data: BillboardColumn
 }
+
 export const CellAction: React.FC<CellActionProps> = ({
   data
 }) => {
   return(
-    <div>Action</div>
+    <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button>
+            <span className="sr-only">Open Menu</span>
+            <MoreHorizontal className="h-4 w-4"/>
+          </Button>
+        </DropdownMenuTrigger>
+    </DropdownMenu>
   )
 }
 
