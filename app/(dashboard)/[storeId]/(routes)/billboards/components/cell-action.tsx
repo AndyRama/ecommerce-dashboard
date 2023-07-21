f5612f0a65cba898794ecfddf4ead77d5bd44b4e
Andy Ramaroson <andyramaroson@gmail.com>
2023-07-21 15:50:36 +0200

    [fix] clean code

--- app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -41,9 +41,9 @@ export const CellAction: React.FC<CellActionProps> = ({
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
+      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
       router.refresh()
-      router.push("/")
+      router.push(`/${params.storeId}/billBoards`)
       toast.success("Billboard deleted.")
     } catch (error) {
       toast.error("Make sure you removed all categories using this billboard first.")
@@ -55,33 +55,39 @@ export const CellAction: React.FC<CellActionProps> = ({
 
   return(
     <>
-    <AlertModal/>
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button  variant="ghost" className="h-8 w-8 p-0">
-          <span className="sr-only">Open Menu</span>
-          <MoreHorizontal className="h-4 w-4"/>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-      <DropdownMenuLabel>
-        Actions
-      </DropdownMenuLabel>
-      <DropdownMenuItem onClick={() => onCopy(data.id)}>
-        <Copy className="mr-2 h-4 w-4"/>
-        CopyId
-      </DropdownMenuItem>
-      <DropdownMenuItem onClick={() => router.push(`/${params.stroreId}/billboards/${data.id}`)}>
-        <Edit className="mr-2 h-4 w-4"/>
-          Update
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+        
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button  variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open Menu</span>
+            <MoreHorizontal className="h-4 w-4"/>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+        <DropdownMenuLabel>
+          Actions
+        </DropdownMenuLabel>
+        <DropdownMenuItem onClick={() => onCopy(data.id)}>
+          <Copy className="mr-2 h-4 w-4"/>
+          CopyId
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => onDelete(data.id)}>
-        <Edit className="mr-2 h-4 w-4"/>
-          Delete
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </>
+        <DropdownMenuItem onClick={() => router.push(`/${params.stroreId}/billboards/${data.id}`)}>
+          <Edit className="mr-2 h-4 w-4"/>
+            Update
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setOpen(true)}>
+          <Edit className="mr-2 h-4 w-4"/>
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   )
 }
 
