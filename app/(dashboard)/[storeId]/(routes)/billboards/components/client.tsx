638b08ac2b929e6b8aa4d21eb1b275349c495d62
Andy Ramaroson <andyramaroson@gmail.com>
2023-07-22 22:39:43 +0200

    [enhancement] update client to dispaly route for API

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -9,6 +9,7 @@ import { useRouter, useParams } from "next/navigation"
 import { DataTable } from "@/components/ui/data-table"
 
 import { BillboardColumn, columns } from "./columns"
+import { ApiList } from "@/components/ui/api-list"
 
 interface BillboardClientProps {
   data: BillboardColumn[]
@@ -33,6 +34,9 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({
       </div>
       <Separator/>
       <DataTable searchKey="label" columns={columns} data={data}/>
+      <Heading title="API" description="API calls for Billboards"/>
+      <Separator/>
+      <ApiList entityName="billboards" entityIdName="billboardId"/>
     </>
   )
 }
