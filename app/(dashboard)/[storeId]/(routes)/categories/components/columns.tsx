022c5791aeb5615722952ae94ec95dc462706ad6
Andy Ramaroson <andyramaroson@gmail.com>
2023-07-23 18:25:23 +0200

    [enhancement] add folder update page categoryColumn

--- app/(dashboard)/[storeId]/(routes)/categories/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/columns.tsx
@@ -1,21 +1,28 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
+
 import { CellAction } from "./cell-action"
 
-export type BillboardColumn = {
+export type CategoryColumn = {
   id: string
-  label: string
+  name: string
+  billboardLabel: string
   createdAt: string
 }
 
-export const columns: ColumnDef<BillboardColumn>[] = [
+export const columns: ColumnDef<CategoryColumn>[] = [
   {
-    accessorKey: "label",
-    header: "Label",
+    accessorKey: "name",
+    header: "Name",
   },
   {
-    accessorKey: "createdAt",
+    accessorKey: "billboard",
+    header: "Billboard",
+    cell:({ row }) => row.original.billboardLabel
+  },
+  {
+    accessorKey: "CreatedAt",
     header: "Date",
   },
   {
