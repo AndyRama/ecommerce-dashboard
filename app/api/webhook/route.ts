bca44e4a89dd0a1420bda5f4f8c614738ead6176
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-06 23:58:50 +0200

    [enhancement] add  webhook for stripe

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,18 @@
+import Stripe from "stripe";
+import { NextResponse } from "next/server";
+
+import { stripe } from "@/lib/stripe";
+import prismadb from "@/lib/prismadb";
+
+export async function POST(Req: Request) {
+  const body = await req.text()
+  const signature = headers().get("Stripe-Signature") as string
+
+  let event: Stripe.Event
+
+  try {
+    event = stripe.webhooks.constructEvent (
+      body,signature,process.env.STRIPE_WEBHOOK_SECRET!
+    )
+  }  
+}
