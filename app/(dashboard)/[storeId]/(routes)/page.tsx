fd40c4ed2d3413a13d16fea5a42a9490c41c4f4c
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-07 22:46:48 +0200

    [Enhancement] add card component and update page overview

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,3 +1,6 @@
+import { Card, CardHeader } from "@/components/ui/card"
+import { Heading } from "@/components/ui/heading"
+import { Separator } from "@/components/ui/separator"
 import prismadb from "@/lib/prismadb"
 
 interface DashboardPageProps {
@@ -7,17 +10,19 @@ interface DashboardPageProps {
 const DashboardPage: React.FC<DashboardPageProps> = async ({
     params
   }) => {
-    const store = await prismadb.store.findFirst({
-      where: {
-        id: params.storeId
-      }
 
-    })
-  
     return (
-     <div>
-      Activestore: {store?.name}
-    </div>
+      <div className="flex-col">
+        <Heading title="Dashboard" description="Overview of your store" />
+        <Separator />
+        <div className="grid gap-4 grid-cols-3" >
+          <Card>
+            <CardHeader>
+              Total Revenue
+            </CardHeader>
+          </Card>
+        </div>
+      </div>
   )
 }
 
