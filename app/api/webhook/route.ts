ba0124caf3c45188730fec69fdd73f15f78ac49e
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-07 22:27:04 +0200

    [Enhancement] condition for order update

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -34,5 +34,20 @@ export async function POST(req: Request) {
   ]
 
   const addressString = addressComponents.filter((c) => c !== null).join(', ') 
-
+  
+  if(event.type === "checkout.session.completed") {
+    const order = await prismadb.order.update({
+      where: {
+        id: session?.metadata?.orderId
+      },
+      data: {
+        isPaid: true,
+        address: addressString,
+        phone: session?.customer_details?.phone || ''
+      },
+      include: {
+        orderItems: true
+      }
+    })
+  }
 }
