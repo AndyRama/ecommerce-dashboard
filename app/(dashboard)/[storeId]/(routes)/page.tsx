14c34501fbd4540fb5b8556346957f4477447048
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-07 23:04:06 +0200

    [Enhancement] add card  for sales

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,9 +1,10 @@
+import { CreditCardIcon, DollarSign, Package } from "lucide-react"
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
 import prismadb from "@/lib/prismadb"
 import { formatter } from "@/lib/utils"
-import { DollarSign } from "lucide-react"
 
 interface DashboardPageProps {
   params: {storeId: string}
@@ -29,6 +30,32 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
               <CardContent>
                 <div className="text-2xl font-bold">
                   {formatter.format(100)}
+                </div>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">
+                  Sales
+                </CardTitle>
+                <CreditCardIcon className="h-4 w-4 text-muted-foreground" />
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">
+                  +25
+                </div>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">
+                  Product In Stoke
+                </CardTitle>
+                <Package className="h-4 w-4 text-muted-foreground" />
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">
+                  15
                 </div>
               </CardContent>
             </Card>
