b4c580e21b5343ad150f33537a3c142d48898c9a
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-07 22:57:16 +0200

    [Enhancement] add card component work

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,7 +1,9 @@
-import { Card, CardHeader } from "@/components/ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
 import prismadb from "@/lib/prismadb"
+import { formatter } from "@/lib/utils"
+import { DollarSign } from "lucide-react"
 
 interface DashboardPageProps {
   params: {storeId: string}
@@ -13,14 +15,24 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
 
     return (
       <div className="flex-col">
-        <Heading title="Dashboard" description="Overview of your store" />
-        <Separator />
-        <div className="grid gap-4 grid-cols-3" >
-          <Card>
-            <CardHeader>
-              Total Revenue
-            </CardHeader>
-          </Card>
+        <div className="flex-1 space-y-4 p-8 pt-6">
+          <Heading title="Dashboard" description="Overview of your store" />
+          <Separator />
+          <div className="grid gap-4 grid-cols-3" >
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">
+                  Total Revenue
+                </CardTitle>
+                <DollarSign className="h-4 w-4 text-muted-foreground" />
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">
+                  {formatter.format(100)}
+                </div>
+              </CardContent>
+            </Card>
+          </div>
         </div>
       </div>
   )
