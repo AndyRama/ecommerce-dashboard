602f3230d7365b17e4987d2f031060d9d5d103d0
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-08 09:16:51 +0200

    [Enhancement] display overview component chart in dashboard

--- components/overview.tsx
+++ components/overview.tsx
@@ -10,12 +10,25 @@ interface OverviewProps {
 export const Overview: React.FC<OverviewProps> = ({
   data
 }) => { 
-  
+
   return (
-    <ResponsiveContainer>
-      <BarChart>
-        <XAxis />
-        <YAxis />
+    <ResponsiveContainer width="100%" height={350} >
+      <BarChart data={data}>
+        <XAxis
+          dataKey="name"
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+        />
+        <YAxis
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+          tickFormatter={(value) => `${value}`}
+        />
+        <Bar dataKey="total" fill="#3498db" radius={[4, 4, 0, 0]} />
       </BarChart>
     </ResponsiveContainer>
   )
