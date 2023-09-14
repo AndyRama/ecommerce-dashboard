6f7d6b3ce443ef82e1f478ebf988da1acaec596c
Andy Ramaroson <andyramaroson@gmail.com>
2023-09-14 17:48:13 +0200

    [fix] error cell-action product

--- app/(dashboard)/[storeId]/(routes)/products/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/cell-action.tsx
@@ -77,7 +77,7 @@ export const CellAction: React.FC<CellActionProps> = ({
           <Copy className="mr-2 h-4 w-4"/>
           CopyId
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => router.push(`/${params.stroreId}/products/${data.id}`)}>
+        <DropdownMenuItem onClick={() => router.push(`/${params.storeId}/products/${data.id}`)}>
           <Edit className="mr-2 h-4 w-4"/>
             Update
           </DropdownMenuItem>
