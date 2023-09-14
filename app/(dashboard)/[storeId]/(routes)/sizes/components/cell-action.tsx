804f46791f529639476ac4a347b74267661b9963
Andy Ramaroson <andyramaroson@gmail.com>
2023-09-14 17:47:06 +0200

    [fix] update cell-action sizes

--- app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
@@ -76,7 +76,7 @@ export const CellAction: React.FC<CellActionProps> = ({
           <Copy className="mr-2 h-4 w-4"/>
           CopyId
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => router.push(`/${params.stroreId}/sizes/${data.id}`)}>
+        <DropdownMenuItem onClick={() => router.push(`/${params.storeId}/sizes/${data.id}`)}>
           <Edit className="mr-2 h-4 w-4"/>
             Update
           </DropdownMenuItem>
